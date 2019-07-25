377fc4091717c20cd11d8c179dcb7a3dc50aa91b
guaiguaiAccount <1242680583@qq.com>
2019-07-25 10:21:14 +0800

    example

---
+++ src/example/basic-type.ts
@@ -0,0 +1,28 @@
+// 布尔类型
+let bool: boolean = false
+
+// 数值类型
+let num: number = 122
+
+// String 类型
+
+let str: string = `fsffdfd${num}`
+
+console.log(str)
+
+// 数组类型
+
+// [1, 2, 3]
+// 写法1
+let arr: number[]
+arr = [1, 2, 4]
+
+// 写法2
+let arr2: Array<number>
+
+let arr3: (number | string)[]
+arr3 = [1, 'fdasf']
+
+
+// 元组类型 固定长度 固定位置上类型的
+let tuple
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-let num: number = 1223
-document.title = `test${num}`
+
+import './example/basic-type'
+let num1: number = 1223
+document.title = `test${num1}`
 
 // let app: Object = document.getElementById('app')
 
